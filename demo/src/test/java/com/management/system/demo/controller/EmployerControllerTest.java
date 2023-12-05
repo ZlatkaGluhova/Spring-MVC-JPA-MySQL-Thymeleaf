1e45bfbb399a3a14beb2161c162973941a616dfa
Zlatka Gluhova <z.t.gluhova@gmail.com>
2023-12-05 22:34:59 +0200

    Add dependencies for tests

---
+++ demo/src/test/java/com/management/system/demo/controller/EmployerControllerTest.java
@@ -0,0 +1,18 @@
+package com.management.system.demo.controller;
+
+import com.management.system.demo.model.Employer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class EmployerControllerTest {
+
+//    @Test
+//    public void saveEmployer(Employer employer){
+//        
+//    }
+
+
+}
