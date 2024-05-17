4eb09d51ae7dd124b3b7514feb83b3a78f9f9ac5
Zlatka Gluhova <z.t.gluhova@gmail.com>
2024-05-17 18:31:24 +0300

    Create Reports PDF

--- demo/src/main/java/com/management/system/demo/service/ReportService.java
+++ demo/src/main/java/com/management/system/demo/service/ReportService.java
@@ -1,9 +1,21 @@
 package com.management.system.demo.service;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import org.springframework.http.HttpEntity;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 
 public interface ReportService {
 
     StringBuilder getReportByUserId(Long id);
 
+    void getPdfReportByUserId(Long id) throws IOException, DocumentException, URISyntaxException;
+
+    HttpEntity<byte[]> generatePdfReportByUserId(Long id) throws DocumentException, IOException, URISyntaxException;
+
+    String createPdfReportByUserId(Long id);
 }
--- demo/src/main/java/com/management/system/demo/utils/FileOutput.java
+++ demo/src/main/java/com/management/system/demo/utils/FileOutput.java
@@ -1,14 +1,20 @@
 package com.management.system.demo.utils;
 
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.management.system.demo.model.Role;
 import com.management.system.demo.model.User;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class FileOutput {
 
@@ -77,7 +83,7 @@ public class FileOutput {
 
             if (mappedDataToString != null) {
 //                csvWriter.newLine();
-                for (String data: mappedDataToString){
+                for (String data : mappedDataToString) {
                     csvWriter.write(data);
                     csvWriter.newLine();
                 }
@@ -91,7 +97,7 @@ public class FileOutput {
         }
         List<String> reportData = new ArrayList<>();
         reportData.add(header);
-        for (String data: mappedDataToString){
+        for (String data : mappedDataToString) {
             reportData.add(data);
         }
 
@@ -115,6 +121,132 @@ public class FileOutput {
 
     public static void printData(String element, Map<String, String> data) {
         data.entrySet()
-                .forEach(entry ->  System.out.printf(element + " with %s: %s is not found!%n", entry.getKey(), entry.getValue()));
+                .forEach(entry -> System.out.printf(element + " with %s: %s is not found!%n", entry.getKey(), entry.getValue()));
     }
+
+    public static Document writeInPDFFile(List<String> usersToString, String headers, File pdfFile) throws DocumentException, IOException, URISyntaxException {
+        Document document = new Document();
+        PdfWriter.getInstance(document, new FileOutputStream(pdfFile));
+
+        document.open();
+
+        PdfPTable table = new PdfPTable(9);
+        addTableHeader(table, headers);
+        addRows(table, usersToString);
+//        addCustomRows(table);
+
+        document.add(table);
+        document.close();
+
+        return document;
+    }
+
+//    public StringBuilder pdfReport(Long id) {
+//        User user = userService.getUserById(id);
+//
+//        List<Role> userRoles = user.getRoles();
+//        List<String> usersToString = new ArrayList<>();
+//
+//        for (int i = 0; i < userRoles.size(); i++) {
+//            String userToString = FileOutput.mappedUserToString(user, i);
+//            usersToString.add(userToString);
+//        }
+//
+//        String header = FileOutput.createHeader(headers);
+//
+//        List<String> reportUser = FileOutput.writeInCSVFile(usersToString, header);
+//
+//        StringBuilder stringBuilder = new StringBuilder();
+//        for (String report : reportUser) {
+//            stringBuilder.append(report);
+//            stringBuilder.append(System.getProperty("line.separator"));
+//        }
+//        return stringBuilder;
+//    }
+
+//    public void metod() throws DocumentException, IOException, URISyntaxException {
+//        Document document = new Document();
+//        PdfWriter.getInstance(document, new FileOutputStream("iTextTable.pdf"));
+//
+//        document.open();
+//
+//        PdfPTable table = new PdfPTable(9);
+//        addTableHeader(table);
+//        addRows(table);
+////        addCustomRows(table);
+//
+//        document.add(table);
+//        document.close();
+//    }
+
+    private static void addTableHeader(PdfPTable table, String headers) {
+        Stream.of(headers.split(";"))
+                .forEach(columnTitle -> {
+                    PdfPCell header = new PdfPCell();
+                    header.setBackgroundColor(BaseColor.LIGHT_GRAY);
+                    header.setBorderWidth(2);
+                    header.setPhrase(new Phrase(columnTitle));
+                    table.addCell(header);
+                });
+    }
+
+    private static void addRows(PdfPTable table, List<String> usersToString) {
+
+        for (String user : usersToString) {
+            String[] strings = user.split(";");
+            for (int i = 0; i < strings.length; i++) {
+                if (!strings[i].equals("")) {
+                    table.addCell(strings[i]);
+                } else
+                    table.addCell("-");
+            }
+        }
+    }
+
+    public static File createDirectory(String directoryPathName, String pdfReports) {
+        File theDir = new File("/" + directoryPathName + "/" + pdfReports);
+        if (!theDir.exists()){
+            theDir.mkdirs();
+        }
+
+        return theDir;
+    }
+
+    public static File createFile(File directory, String pdfReportName) {
+        File theDir = new File(directory, pdfReportName);
+//        if (!theDir.exists()){
+//            theDir.mkdirs();
+//        }
+
+        return theDir;
+
+    }
+
+
+//    public static byte[] toByteArray(FileInputStream fileInputStream) {
+//        byte[] bArr = ByteBuffer.wrap(FileUtils.readFileToByteArray(new File(filePathToVeryLargeFileInGBs)));
+//    }
+
+//        table.addCell("row 1, col 1");
+//        table.addCell("row 1, col 2");
+//        table.addCell("row 1, col 3");
 }
+
+//    private void addCustomRows(PdfPTable table)
+//            throws URISyntaxException, BadElementException, IOException {
+//        Path path = Paths.get(ClassLoader.getSystemResource("Java_logo.png").toURI());
+//        Image img = Image.getInstance(path.toAbsolutePath().toString());
+//        img.scalePercent(10);
+//
+//        PdfPCell imageCell = new PdfPCell(img);
+//        table.addCell(imageCell);
+//
+//        PdfPCell horizontalAlignCell = new PdfPCell(new Phrase("row 2, col 2"));
+//        horizontalAlignCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+//        table.addCell(horizontalAlignCell);
+//
+//        PdfPCell verticalAlignCell = new PdfPCell(new Phrase("row 2, col 3"));
+//        verticalAlignCell.setVerticalAlignment(Element.ALIGN_BOTTOM);
+//        table.addCell(verticalAlignCell);
+//    }
+
